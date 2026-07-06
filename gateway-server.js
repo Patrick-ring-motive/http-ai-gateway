b2752be93c54e42f1008be60696028af21720041
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-07-06 11:21:51 -0500

    Implement gateway server with request normalization

---
+++ gateway-server.js
@@ -0,0 +1,49 @@
+import { env } from "cloudflare:workers";
+
+const {PROVIDER_KEY} = env;
+const gatewayHost = /gateway\.ai\.cloudflare\.com/gi;
+const gatewayPrefix =/^https:\/\/gateway\.ai\.cloudflare\.com\/v1\/[^\/]+\/[^\/]+\/[^\/]+\//gi;
+
+const normalizeRequest = (request)=>{
+    const url = new URL(request.url);
+    const reqHost = RegExp(request.headers.get('x-gateway-source'),'gi');
+    const reqHeaders = new Headers(request.headers);
+    reqHeaders.delete('x-gateway-source');
+    
+    for(const [key,value] of reqHeaders){
+      reqHeaders.set(key,value
+        .replaceAll(gatewayPrefix,url.origin+'/')
+        .replaceAll(gatewayHost,url.host)
+        .replaceAll(reqHost,url.host)
+      );
+    }
+    const requestInit = {
+      method: request.method,
+      headers: reqHeaders,
+    };
+    if(request.body){
+      requestInit.body = request.body;
+    }
+    return new Request(request.url,requestInit);
+};
+
+const gatewayServer =  async(request)=> {
+    if(request.headers.get('x-provider-key') !== PROVIDER_KEY){
+      return new Response(null,{status:403});
+    }
+    request = normalizeRequest(request);
+
+    //do request logic
+
+    return new Response("my response",{
+      headers:{
+        "x-gateway-target":new URL(request.url).host
+      }
+    });
+  };
+
+export default {
+  async fetch(request) {
+    return gatewayServer(request);
+  },
+};
